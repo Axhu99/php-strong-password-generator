f1b7a2d1d21ec9974eb649aa9e5cb2845289a91b
Axhu99 <xhuliodreni@icloud.com>
2024-02-21 16:49:53 +0100

    added first 'function'

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-
+if (!empty($_GET)) {
+    $comb = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $pass = [];
+    $comb_len = strlen($comb) - 1;
+    for ($i = 0; $i < $_GET['lenght_password']; $i++) {
+        $n = rand(0, $comb_len);
+        $pass[] = $comb[$n];
+    }
+    print(implode($pass));
+}
 ?>
 
 
@@ -17,7 +26,7 @@
     <form action="" method="GET">
         <!-- Input di tipo NUMBER dove chiedo la lunghezza della password  -->
         <label for="lenght_password" class="form-label">Lunghezza della password</label>
-        <input type="number" class="form-control" id="lenght_password">
+        <input type="number" class="form-control" id="lenght_password" name="lenght_password">
 
     </form>
 </body>

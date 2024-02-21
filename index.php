eee4342ac186f5e1b540ba163cd96aa2388272d4
Axhu99 <xhuliodreni@icloud.com>
2024-02-21 16:32:35 +0100

    added tag form

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- FORM -->
+    <form action="" method="GET">
+        <!-- Input di tipo NUMBER dove chiedo la lunghezza della password  -->
+        <label for="lenght_password" class="form-label">Lunghezza della password</label>
+        <input type="number" class="form-control" id="lenght_password">
+
+    </form>
+</body>
+
+</html>

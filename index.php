21c3386b03a5493b0cbd06d82db53e67effa80b1
Axhu99 <xhuliodreni@icloud.com>
2024-02-23 01:27:56 +0100

    Added password function

--- index.php
+++ index.php
@@ -1,16 +1,34 @@
 <?php
-if (!empty($_GET)) {
-    $comb = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-    $pass = [];
-    $comb_len = strlen($comb) - 1;
-    for ($i = 0; $i < $_GET['lenght_password']; $i++) {
-        $n = rand(0, $comb_len);
-        $pass[] = $comb[$n];
+
+$length = $_GET['lenght_password'] ?? '';
+
+if (!empty($length)) {
+    $password = password_generator($length);
+}
+
+function password_generator($length)
+{
+    $password = '';
+
+    $letters = 'abcdefghijklmnopqrstuvwxyz';
+    $numbers = '0123456789';
+    $symbols = '!@#$%^&*()_+';
+
+    $characters = $letters . strtoupper($letters) . $numbers . $symbols;
+
+    $total_characters = mb_strlen($characters);
+
+    while (mb_strlen($password) < $length) {
+        $random_index = rand(0, $total_characters - 1);
+
+        $random_character = $characters[$random_index];
+
+        $password .= $random_character;
     }
-    print(implode($pass));
+
+    return $password;
 }
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,17 +36,36 @@ if (!empty($_GET)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <title>Document</title>
 </head>
 
-<body>
-    <!-- FORM -->
-    <form action="" method="GET">
-        <!-- Input di tipo NUMBER dove chiedo la lunghezza della password  -->
-        <label for="lenght_password" class="form-label">Lunghezza della password</label>
-        <input type="number" class="form-control" id="lenght_password" name="lenght_password">
+<body class="bg-primary-subtle" data-bs-theme="dark">
+    <div class="container text-center">
+        <div>
+            <h1 class="text-secondary">Strong Password Generetor</h1>
+            <h2>Genera una password sicura</h2>
+        </div>
+        <?php if (isset($password)) : ?>
+            <div class="alert alert-info">
+                La password generata e' : <?= $password ?>
+            </div>
+        <?php endif;  ?>
+        <!-- FORM -->
+        <form action="" method="GET" class="bg-light p-2">
+            <!-- Input di tipo NUMBER dove chiedo la lunghezza della password  -->
+            <div class="row text-start">
+                <div class="col-8">
+                    <label for="lenght_password" class="form-label">Lunghezza della password:</label>
+                </div>
+                <div class="col-4">
+                    <input type="number" class="form-control" id="lenght_password" name="lenght_password" min="7" value="<?= $length ?? 7 ?>">
+                </div>
+            </div>
 
-    </form>
+            <button type="submit">invia</button>
+        </form>
+    </div>
 </body>
 
 </html>
